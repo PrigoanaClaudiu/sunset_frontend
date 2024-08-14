d6501a0ca979676bb11910b9fb545249138da351
PrigoanaClaudiu <claudiu_prigoana16@yahoo.com>
2024-08-14 17:17:53 +0300

    location pathname script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if (!document.getElementById('auth-page') && window.location.pathname !== '../index.html') {
+    if (!document.getElementById('auth-page') && window.location.pathname !== '/sunset_frontend/index.html') {
         fetch('header.html')
             .then(response => response.text())
             .then(data => {
